9e98e93cde0fdfd66c35ea3f085222ba81cf1388
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2023-04-09 20:42:18 +0800

    db connect

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express();
 import UserRouter from './api/user.js'
@@ -7,18 +8,29 @@ import GameRouter from './api/game.js'
 import BlogRouter from './api/blog.js'
 import helper from "./functions.js"
 
-import db from "./mongodb_helper.js"
 
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-app.use(bodyParser.json({limit: '50mb'}))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(cors({orogin: '*'}))
 
-app.use('/api/user', UserRouter)
-app.use('/api/map', MapRouter)
-app.use('/api/game', GameRouter)
-app.use('/api/blog', BlogRouter)
+mongoose.connect('mongodb+srv://Pacman:1234@pacman.0bmd8dr.mongodb.net/?retryWrites=true&w=majority');
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log('DB connection successful');
+    app.use(bodyParser.json({limit: '50mb'}))
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(cors({orogin: '*'}))
 
-app.listen(8007, '127.0.0.1')
+    app.use('/api/user', UserRouter)
+    app.use('/api/map', MapRouter)
+    app.use('/api/game', GameRouter)
+    app.use('/api/blog', BlogRouter)
+});
+
+
+
+// start the server
+app.listen(3000);
+
+
+
