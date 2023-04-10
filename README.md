25ab6be038aa6586ac6aab2a0e28f901451af7e5
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2023-04-10 15:58:41 +0800

    initial

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PacMan3Dbackend
+
+## Technologies Used
+
+MongoDB: A popular NoSQL database that provides a flexible, scalable, and document-oriented data model, making it suitable for handling game-related data.
+
+## Setting up the Database
+
+To set up the Pacman 3D Database on your local environment, follow these steps:
+
+1. Install MongoDB on your machine and start the MongoDB server.
+2. Clone this repository to your local machine using git clone, create a pacman database in your local database.
+3. Navigate to the cloned directory and install the dependencies using npm install.
+4. Start the application using node server.js or npm start. The application will be running at http://localhost:3000 by default.
--- src/api/user.js
+++ src/api/user.js
@@ -5,37 +5,40 @@ const UserRouter = new Router();
 import { createHash } from 'node:crypto'
 import db from "../mongodb_connection.js"
 
+
+//Function: createUser
 UserRouter.post('/user', async (req, res) => {
-    console.log("try2")
-    let username = req.body.username
-    let password = req.body.password
-    let hashed_password = createHash('sha3-256').update(password).digest('hex')
-    const userExisted = await db.collection("users").findOne({username: username})
-        if(userExisted != null){
-            res.send({ isCreated: false
-            , error_message: "User already exists", error_code: 3 })
-                console.log("try3")
-        }else{
-            console.log("try4")
-            const result = await db.collection("users").insertOne({username : username, password: hashed_password})
-            if (!result) res.send({ isCreated: false, error_message: "Failed to create user", error_code: 4 })
-            else res.send({ isCreated: true })
-        }      
-    
-})
+    try {
+        let username = req.body.username;
+        let password = req.body.password;
+        let hashed_password = createHash('sha3-256').update(password).digest('hex');
+        const userExisted = await db.collection("users").findOne({ username: username });
+
+        if (userExisted != null) {
+            res.send({ isCreated: false, error_message: "User already exists"});
+        } else {
+            const result = await db.collection("users").insertOne({ username: username, password: hashed_password });
+            if (!result) {
+                res.send({ isCreated: false, error_message: "Failed to create user"});
+            } else {
+                res.send({ isCreated: true });
+            }
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        res.status(500).send({ isCreated: false, error_message: "Internal server error"});
+    }
+});
+
+//Function: retrieveUser
 UserRouter.get('/user/:username', async (req, res) => {
-    console.log("get act!");
     let username = req.params.username
     const result = await db.collection("users").findOne({"username" : username})
     if (!result) res.send(null)
     else res.send(result)
 })
 
-UserRouter.get('/*', async (req, res) => {
-    console.log("get act!");
-     res.send('HELLO WORLD');
-})
-
+//Function: deleteUser
 UserRouter.delete("/user/:username", async (req, res) => {
     const username = req.params.username
     const result = await db.collection("users").deleteOne({ "username": username })
