65b55220f44fa16b69e60f32b99bd392bb6ec989
Wilson Leung <wilson.leung@sas.com>
2023-04-12 10:08:07 +0800

    connected db to monogodb cloud

--- src/mongodb_connection.js
+++ src/mongodb_connection.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 
-const url = "mongodb://localhost:27017/pacman"
+const url = "mongodb+srv://sad9jai:04875398MON@csci3100.9m8rnfx.mongodb.net/pacman?retryWrites=true&w=majority"
 const db = mongoose.connection;
 mongoose.connect(url, {})
     .then(result => console.log("database connected"))
